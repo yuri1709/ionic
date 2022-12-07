0c712cbb0320428ed7072c117e4c3a4883a01e8b
Técnico em Informática 2021.2 <36127512021.2@senacrjedu.local>
2022-12-07 11:23:15 -0300

    banco de dados

---
+++ app01/src/app/core/servicos/banco.service.ts
@@ -0,0 +1,35 @@
+import { HttpClient, HttpHeaders } from '@angular/common/http';
+import { Injectable } from '@angular/core';
+import { Produto } from '../models/produtos.model';
+
+@Injectable({
+  providedIn: 'root'
+})
+export class BancoService {
+
+  httpOptions = {
+    headers: new HttpHeaders({
+      'Content-Type' : 'application/json'
+    })
+  }
+
+  readonly API = " http://localhost:3000/produtosArray/"
+
+  constructor(private http: HttpClient) { }
+
+  insertItem(produto: any) {
+    return this.http.post(this.API, JSON.stringify(produto), this.httpOptions).subscribe();
+  }
+
+  getAllItem() {
+    return this.http.get<Produto[]>(this.API)
+  }
+
+  deleteItem(id: number) {
+    return this.http.delete(this.API + id).subscribe()
+  }
+
+
+
+
+}
--- app01/src/app/page/home/home.page.ts
+++ app01/src/app/page/home/home.page.ts
@@ -2,6 +2,7 @@ import { Component, OnInit } from '@angular/core';
 import { Produto } from '../../core/models/produtos.model';
 import { HttpClient } from '@angular/common/http';
 import { AlertController, AlertInput, iosTransitionAnimation, LoadingController } from '@ionic/angular';
+import { BancoService } from 'src/app/core/servicos/banco.service';
 @Component({
   selector: 'app-home',
   templateUrl: 'home.page.html',
@@ -12,44 +13,45 @@ export class HomePage implements OnInit {
   itens: Produto[] = []
 
   constructor(
-    private http: HttpClient,
-
     //loadingController - Ferramenta do carregando.
     private loadCtrl: LoadingController,
 
-    private alertCtrl: AlertController
+    private alertCtrl: AlertController,
+
+    private db: BancoService
     ) {}
   
   ngOnInit() {
     //Carregando o inicio da página
-    this.carregando()
+    this.carregando(1, "Loading...")
     this.getJson()
   }
 
   getJson() {
-    this.http.get<Produto[]>("http://localhost:3000/produtosArray").subscribe(resultado => this.itens = resultado)
+    this.db.getAllItem().subscribe(resultado => this.itens = resultado)
     console.log("itens:" + this.itens)
   }
 
-  async carregando() {
-    const load = this.loadCtrl.create({
-      mode: 'ios',
-      message: 'Aguarde...',
-      duration: 1000
-    });
-    (await load).present();
+  deletarItem(id: number, nome: string) {
+    try{
+      this.db.deleteItem(id);
+      this.carregando(5000,"Deletando item "+nome+"...");
+    }
+    finally {
+      location.reload();
+    }
+    
   }
 
   //Método do alertando
   async alertando() {
     const alert = this.alertCtrl.create({
       mode: 'ios',
-      message: 'Sua compra',
-      header: 'Lista:',
-      buttons: ['ok'],
+      header: 'Cadastrar Produto',
+      message: 'Preencha os campos abaixo:',
       inputs: [
         {
-          name: 'produto',
+          name: 'item',
           type: 'text',
           placeholder: 'Informe o produto'
         },
@@ -57,10 +59,52 @@ export class HomePage implements OnInit {
           name: 'quantidade',
           type: 'text',
           placeholder: 'Informe a quantidade'
+        },
+        {
+          name: 'preco',
+          type: 'number',
+          placeholder: 'Informe o preço'
         }
+      ],
+      buttons: [     
+        {
+          text: 'Cancelar',
+          handler: () => {
+          console.log("Item cancelado")
+          }
+        },
+        { 
+          text: 'Cadastrar',
+          handler: (form) => { //é como se fosse um treinador, ele trabalha como um submit().
+            //vamos criar um objeto que irá formar nosso item da lista
+            let item = {
+              nome: form.item,
+              quantia: form.quantidade,
+              preco: form.preco
+            };
+            try {
+              console.log(item)
+              this.db.insertItem(item);
+              this.carregando(6000, "Cadastrando...");
+            } finally {
+              location.reload();
+            }
+            
+          }   
+        } 
       ]
     });
     (await alert).present();
+  }
+
+  
+  async carregando(timer: number, msg:string) {
+    const load = this.loadCtrl.create({
+      mode: 'ios',
+      message: msg,
+      duration: timer
+    });
+    (await load).present();
   }
   
 }
